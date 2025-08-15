c9ea066eaef5b3f5bff0720ede4f2ec1b4344922
jose-junior-maker <jose_juniorr@hotmail.com>
2025-08-15 16:13:41 -0300

    Configurando react-router-dom e adicionando primeiras rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Listing from "./rotas/Listing"
+import Form from "./rotas/Form"
 
 function App() {
   
   return (
-    <Navbar />
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Listing />} />
+          <Route path="/form">
+            <Route path=":movieId" element={<Form />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
+    
   )
 }
 
