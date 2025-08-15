547858ddd8d952796af0642cf1195b3ebb2a14b8
jose-junior-maker <jose_juniorr@hotmail.com>
2025-08-15 14:55:20 -0300

    Adicionado componente navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import githubIcon from '../../assets/img/github.svg'
+import './styles.css'
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/jose-junior-maker">
+                        <div className='dsmovie-contact-container'>
+                            <img src={githubIcon} alt="github" />
+                            <p className='dsmovie-contact-link'>/jose-junior-maker</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
