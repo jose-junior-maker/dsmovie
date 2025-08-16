52704b9dfe1d577d8d77ba38642278214982dd68
jose-junior-maker <jose_junior@hotmail.com>
2025-08-16 14:42:47 -0300

    Adicionando configuração de segurança

---
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsmovie.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Liberação do H2-console no profile "test"
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+        }
+
+        http
+            .cors(cors -> {}) // habilita CORS
+            .csrf(csrf -> csrf.disable()) // desabilita CSRF
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // sem sessão
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // libera tudo
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
